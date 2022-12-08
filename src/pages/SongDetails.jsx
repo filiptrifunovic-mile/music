fb24b5eeec612cd6cbd36b04ad31c9679b3e50d3
Filip <filip.trifunovic23@gmail.com>
2022-12-08 23:15:43 +0100

    details header display info

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -19,15 +19,18 @@ const SongDetails = () => {
       <div className="mb-10">
         <h2 className="text-white text-3xl font-bold">Lyrics: </h2>
         <div className="mt-5">
-          {songData?.sections[1]?.type === "LYRICS" ? (
+          {songData?.sections[1].type === "LYRICS" ? (
             songData?.sections[1]?.text.map((line, i) => (
-              <p key={i} className="text-gray-400 text-base my-1">
+              <p
+                key={`lyrics-${line}-${i}`}
+                className="text-gray-400 text-base my-1"
+              >
                 {line}
               </p>
             ))
           ) : (
             <p className="text-gray-400 text-base my-1">
-              Sorry, no lyrics found!
+              Sorry, No lyrics found!
             </p>
           )}
         </div>
