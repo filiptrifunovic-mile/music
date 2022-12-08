f51d36dce8c779a790f065683b4499419264bd92
Filip <filip.trifunovic23@gmail.com>
2022-12-08 14:55:27 +0100

    play/pause functionality

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -4,11 +4,17 @@ import { useDispatch } from "react-redux";
 import PlayPause from "./PlayPause";
 import { playPause, setActiveSong } from "../redux/features/playerSlice";
 
-const SongCard = ({ song, i }) => {
-  const activeSong = "test";
+const SongCard = ({ song, i, isPlaying, activeSong, data }) => {
+  const dispatch = useDispatch();
 
-  function handlePauseClick() {}
-  function handlePlayClick() {}
+  function handlePauseClick() {
+    dispatch(playPause(false));
+  }
+
+  function handlePlayClick() {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  }
 
   return (
     <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
@@ -24,6 +30,8 @@ const SongCard = ({ song, i }) => {
             song={song}
             handlePause={handlePauseClick}
             handlePlay={handlePlayClick}
+            isPlaying={isPlaying}
+            activeSong={activeSong}
           />
         </div>
         <img src={song.images?.coverart} alt="song-img" />
