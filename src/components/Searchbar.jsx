076465a9be40049373ba0a1e0cfdd6169b4392f5
Filip <filip.trifunovic23@gmail.com>
2022-12-09 16:48:30 +0100

    fixes

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+
 import { FiSearch } from "react-icons/fi";
 
 const Searchbar = () => {
   const navigate = useNavigate();
   const [searchTerm, setSearchTerm] = useState("");
 
-  function handleSubmit(e) {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
     navigate(`/search/${searchTerm}`);
-  }
+  };
 
   return (
     <form
@@ -19,19 +20,19 @@ const Searchbar = () => {
       className="p-2 text-gray-400 focus-within:text-gray-600"
     >
       <label htmlFor="search-field" className="sr-only">
-        Search all songs
+        Search all files
       </label>
-      <div className="flex flex-row justify-start items-start">
-        <FiSearch className="w-5 h-5 ml-4" />
+      <div className="flex flex-row justify-start items-center">
+        <FiSearch aria-hidden="true" className="w-5 h-5 ml-4" />
         <input
           name="search-field"
           autoComplete="off"
           id="search-field"
-          placeholder="search"
+          className="flex-1 bg-transparent border-none placeholder-gray-500 outline-none text-base text-white p-4"
+          placeholder="Search"
           type="search"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
-          className="flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-base text-white p-4"
         />
       </div>
     </form>
